7b5e97acd39720839d8e9b2075256264cb5aeeae
Viliam Holub <viliam@dummy.example.com>
2008-09-30 16:42:23 +0000

    Added empty classes FlyRules and MatchResult

---
+++ software/CLO/src/java/ie/ucd/clops/runtime/structs/FlyRules.java
@@ -0,0 +1,7 @@
+package ie.ucd.clops.runtime.structs;
+
+/**
+ * TODO; to make compiler happy
+ */
+public class FlyRules {
+}
---
+++ software/CLO/src/java/ie/ucd/clops/runtime/structs/MatchResult.java
@@ -0,0 +1,20 @@
+package ie.ucd.clops.runtime.structs;
+
+/**
+ * Dummy class to make compiler happy
+ * TODO
+ */
+public class MatchResult {
+
+	public int getNumberOfArgsConsumed() {
+		return 0; //TODO
+	}
+
+	public Exception getErrorMessage() {
+		return null; //TODO
+	}
+
+	public boolean errorDuringProcessing() {
+		return false; //TODO
+	}
+}

51c0d822cf9fa4f6185db0da2044640b829a87db
Fintan Fairmichael <fintanf@dummy.example.com>
2008-10-14 16:12:18 +0000

    Adding CodeGenerator sketch.

---
+++ software/CLO/src/java/ie/ucd/clops/codegeneration/CodeGenerator.java
@@ -0,0 +1,25 @@
+package ie.ucd.clops.codegeneration;
+
+import ie.ucd.clops.dsl.structs.OptionDescription;
+
+import java.io.File;
+import java.util.Set;
+
+public class CodeGenerator {
+
+  
+  //Input: OptionDescriptions
+  //       Directory to output to
+  //Ouput: Some .java files
+  
+  public static void createCode(Set<OptionDescription> opDescriptions, File outputDir) {
+    
+    JavaClass specificParser = new JavaClass();
+    
+    Method optionMethod = specificParser.addMethod("createOptions", "", "Options");
+    
+    //etc.
+    
+  }
+  
+}

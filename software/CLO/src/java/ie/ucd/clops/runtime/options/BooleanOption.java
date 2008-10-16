f0a2b52d5cdf211b99b17f9880006c7ceff98227
Mikolas Janota <mikolas@dummy.example.com>
2008-10-16 18:27:20 +0000

    Adding a BooleanOption.

---
+++ software/CLO/src/java/ie/ucd/clops/runtime/options/BooleanOption.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package ie.ucd.clops.runtime.options;
+
+import ie.ucd.clops.runtime.parser.ProcessingResult;
+
+import java.util.Set;
+
+/**
+ * @author Mikolas Janota
+ *
+ */
+public class BooleanOption extends BasicOption {
+  private Boolean value;
+    
+  public BooleanOption(final Set<String> aliases) {
+     super(aliases);
+  }
+  
+  /* (non-Javadoc)
+   * @see ie.ucd.clo.runtime.options.Option#getType()
+   */
+  public  OptionType getType() { return OptionType.BOOLEAN; }
+
+  /* (non-Javadoc)
+   * @see ie.ucd.clo.runtime.options.Option#getValue()
+   */
+  public Object getValue() { return value; }
+
+  /* (non-Javadoc)
+   * @see ie.ucd.clo.runtime.options.Option#hasValue()
+   */
+  public boolean hasValue() {
+     return value != null;
+  }
+
+  /* (non-Javadoc)
+   * @see ie.ucd.clo.runtime.options.Option#match(java.lang.String[], int)
+   */
+  public ProcessingResult process(String[] args, int offset) {
+     String currentArg = args[offset];
+     if (getAliases().contains(currentArg)) {
+        this.value = true;
+        return new ProcessingResult(1, false, null);
+     } else {
+        return null;
+     }
+  }
+
+  /* (non-Javadoc)
+   * @see ie.ucd.clo.runtime.options.Option#set(java.lang.Object)
+   */
+  public void set(Object value) {
+     this.value = (Boolean)value;
+  }
+
+  /* (non-Javadoc)
+   * @see ie.ucd.clo.runtime.options.Option#unset()
+   */
+  public void unset() {
+     this.value = null;
+  }
+
+  /* (non-Javadoc)
+   * @see ie.ucd.clo.runtime.options.IMatchabel#getMatchingOption(java.lang.String)
+   */  
+  public Option getMatchingOption(String argument) {
+     return getAliases().contains(argument) ? this : null;
+  }
+
+
+}

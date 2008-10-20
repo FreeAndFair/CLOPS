2e4634348b543c8cfba6f264dd08f24a2ba47cf1
Mikolas Janota <mikolas@dummy.example.com>
2008-10-20 20:31:34 +0000

    Adding a comment.

--- software/CLO/src/java/ie/ucd/clops/runtime/options/IMatchString.java
+++ software/CLO/src/java/ie/ucd/clops/runtime/options/IMatchString.java
@@ -1,6 +1,17 @@
 package ie.ucd.clops.runtime.options;
 
+
+/**
+ * An interface that is used to obtain instances of 
+ * the {@code IMatchable} interface from a {@code String}.
+ *
+ * @author Mikolas Janota
+ */
 public interface IMatchString  {
-  IMatchable getMatchable(String param);
+
+    /**
+     * Obtain {@code IMatchable} associated with the given {@code String}.
+     */
+    IMatchable getMatchable(/*@non_null*/String param);
 }
 

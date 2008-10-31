320e05bf4e759fe59162d1e717a72ba6aedf1a9b
Mikolas Janota <mikolas@dummy.example.com>
2008-10-31 11:13:51 +0000

    Formatting and amending comments.

--- software/CLO/src/java/ie/ucd/clops/runtime/automaton/Token.java
+++ software/CLO/src/java/ie/ucd/clops/runtime/automaton/Token.java
@@ -3,20 +3,21 @@ package ie.ucd.clops.runtime.automaton;
 
 
 /**
- * Representation of a token in the format.
+ * Representation of a token in the regular expression (command line format).
+ * @author Viliam Holub
+ * @param T the type of the entity that has generated this token
  */
 public class Token<T> {
 	TokenType type;
 	T match;
 
 	public Token( TokenType type) {
-		this.type = type;
+            this.type = type;
 	}
 
-	public Token( /*@ non_null @*/ TokenType type,
-			/*@ non_null @*/ T match)
-	{
-		this.type = type;
-		this.match = match;
+	public Token(/*@ non_null @*/ TokenType type,
+		     /*@ non_null @*/ T match) {
+            this.type = type;
+            this.match = match;
 	}
 }

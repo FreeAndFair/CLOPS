9486dcead4ba5441aa2d0ba9f3c7feec066a5f1c
Mikolas Janota <mikolas@dummy.example.com>
2008-11-07 15:10:45 +0000

    Adding comments and some JML.

--- software/CLO/src/java/ie/ucd/clops/runtime/options/Option.java
+++ software/CLO/src/java/ie/ucd/clops/runtime/options/Option.java
@@ -5,6 +5,8 @@ import ie.ucd.clops.runtime.parser.ProcessingResult;
 import java.util.Set;
 
 /**
+ * This interface describes information about the option needed during option parsing.
+ *
  * @author Mikolas Janota
  * @author Fintan
  *
@@ -13,28 +15,29 @@ public interface Option extends IMatchable {
   
   /**
    * Match this option against the arguments starting at the given offset.
-   * Returns a ProcessingResult indicating whether there was an error in processing
-   * If there was an error an error message is contained in the ProcessingResult.
-   * If the processing was successful, the ProcessingResult indicates how many arguments
+   * Returns a {@code ProcessingResult} indicating whether there was an error in the processing.
+   * If there was an error, an error message is contained in the ProcessingResult.
+   * If the processing was successful, the {@code ProcessingResult} indicates how many arguments
    * were consumed.
    * @param args
    * @param offset
-   * @return a ProcessingResult indicating the result of this processing.
+   * @return a {@code ProcessingResult} indicating the result of this processing
    */
-  ProcessingResult process(String[] args, int offset);
+   //@ requires 0 <= offset && offset < args.length;
+   ProcessingResult process(/*@non_null*/String[] args, int offset);
   
   /**
    * Does this Option have a value?
    * @return whether or not the option has a value set
    */
-  boolean hasValue();
+   /*@pure*/boolean hasValue();
   
   /**
    * Get the value associated with this Option.
    * {@code hasValue() <=> getValue()!=null}.
    * @return the value associated with this Option.
    */
-  Object getValue();
+   /*@pure*/Object getValue();
   
   /**
    * Unset this Option.
@@ -71,5 +74,5 @@ public interface Option extends IMatchable {
   void setProperty(/*@non_null*/String propertyName, String propertyValue) throws InvalidOptionPropertyValueException;
   
    /** Determines whether this option supports the given property. */
-   boolean acceptsProperty(/*@non_null*/String propertyName);
+   /*@pure*/boolean acceptsProperty(/*@non_null*/String propertyName);
 }

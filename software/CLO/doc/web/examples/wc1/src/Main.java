3f643f98fe7a62a31d599ad72fbcc3981e86cc89
Radu Grigore <rgrig@dummy.example.com>
2008-12-15 17:17:22 +0000

    wc with dash-dash

---
+++ software/CLO/doc/web/examples/wc1/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import generated.WcParser;
+import generated.WcOptionsInterface;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    WcParser parser = new WcParser();
+    if (!parser.parse(args)) {
+      System.out.println("Usage: java Main [OPTIONS] file...");
+      System.exit(1);
+    }
+    WcOptionsInterface opt = parser.getOptionStore();
+    if (opt.isWordsSet())
+      System.out.println("I should print a word count.");
+    if (opt.isBytesSet())
+      System.out.println("I should print a byte count.");
+    if (opt.isFilesSet()) {
+      File f = opt.getFiles();
+      System.out.print("The file " + f.getAbsolutePath());
+      if (f.exists())
+        System.out.println(" exists.");
+      else
+        System.out.println(" does not exist.");
+    }
+  }
+}
+

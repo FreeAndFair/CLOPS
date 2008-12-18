5b972604d90afb18859db1bb80c69bcf04a5e1b8
Radu Grigore <rgrig@dummy.example.com>
2008-12-18 19:34:33 +0000

    a little bla-bla

--- software/CLO/doc/web/examples/wc/src/Main.java
+++ software/CLO/doc/web/examples/wc/src/Main.java
@@ -10,17 +10,17 @@ public class Main {
       System.exit(1);
     }
     WcOptionsInterface opt = parser.getOptionStore();
-    if (opt.isWordsSet())
-      System.out.println("I should print a word count.");
-    if (opt.isBytesSet())
-      System.out.println("I should print a byte count.");
-    for (File f : opt.getFiles()) {
-      System.out.print("The file " + f.getAbsolutePath());
-      if (f.exists())
-        System.out.println(" exists.");
-      else
-        System.out.println(" does not exist.");
-    }
+    if (opt.isWordsSet()) System.out.println("I should print a word count.");
+    if (opt.isBytesSet()) System.out.println("I should print a byte count.");
+    for (File f : opt.getFiles()) checkFile(f);
+  }
+
+  public static void checkFile(File f) {
+    System.out.print("The file " + f.getPath());
+    if (f.exists())
+      System.out.println(" exists.");
+    else
+      System.out.println(" does not exist.");
   }
 }
 

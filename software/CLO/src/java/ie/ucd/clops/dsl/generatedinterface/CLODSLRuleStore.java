fae8c7aca2f4c3e4decb4bf811622865fd37282b
Radu Grigore <rgrig@dummy.example.com>
2009-01-23 15:46:04 +0000

    (1) prettify (2) validity checks in clo-dsl

--- software/CLO/src/java/ie/ucd/clops/dsl/generatedinterface/CLODSLRuleStore.java
+++ software/CLO/src/java/ie/ucd/clops/dsl/generatedinterface/CLODSLRuleStore.java
@@ -9,9 +9,39 @@ public class CLODSLRuleStore extends ie.ucd.clops.runtime.rules.RuleStore {
     
 
   
+    Expression<Boolean> rule1Condition = new Rule1Condition();
+    ValidityRule rule1 = new ValidityRule(rule1Condition);
+    rule1.addAction(new Action<java.util.List<String>>("CLOPSERROROPTION", new Rule1Expression()));
+    addValidityRule(rule1);
+    Expression<Boolean> rule2Condition = new Rule2Condition();
+    ValidityRule rule2 = new ValidityRule(rule2Condition);
+    rule2.addAction(new Action<java.util.List<String>>("CLOPSERROROPTION", new Rule2Expression()));
+    addValidityRule(rule2);
   }
 
 
   
+    public static class Rule1Condition implements Expression<Boolean> {
+      public Boolean evaluate(OptionStore optionStore) {
+        return !((ie.ucd.clops.runtime.options.FileOption)optionStore.getOptionByIdentifier("input")).hasValue();
+      }
+    }
+    
+    public static class Rule1Expression implements Expression<java.util.List<String>> {
+      public java.util.List<String> evaluate(OptionStore optionStore) {
+        return java.util.Arrays.asList("input not set");
+      }
+    }
+    public static class Rule2Condition implements Expression<Boolean> {
+      public Boolean evaluate(OptionStore optionStore) {
+        return !((ie.ucd.clops.runtime.options.FileOption)optionStore.getOptionByIdentifier("output")).hasValue();
+      }
+    }
+    
+    public static class Rule2Expression implements Expression<java.util.List<String>> {
+      public java.util.List<String> evaluate(OptionStore optionStore) {
+        return java.util.Arrays.asList("output not set");
+      }
+    }
 
 }

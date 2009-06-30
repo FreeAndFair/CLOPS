9a5fc1791256eb9a6c9ed2ef8ad587bfcdb9e0e6
jcharles <jcharles@dummy.example.com>
2009-06-30 16:26:20 +0000

    fixed a missing dependency

--- src/java/ie/ucd/clops/runtime/ClopsInternalException.java
+++ src/java/ie/ucd/clops/runtime/ClopsInternalException.java
@@ -4,5 +4,14 @@ public class ClopsInternalException extends RuntimeException {
 
   /** */
   private static final long serialVersionUID = 1L;
+  public  ClopsInternalException(String message, Throwable cause) {
+    super(message, cause);
+  }
 
+  public  ClopsInternalException(String message) {
+    super(message);
+  }
+  public  ClopsInternalException() {
+    super();
+  }
 }
--- src/java/ie/ucd/clops/runtime/options/exception/InvalidOptionPropertyValueException.java
+++ src/java/ie/ucd/clops/runtime/options/exception/InvalidOptionPropertyValueException.java
@@ -1,11 +1,13 @@
 package ie.ucd.clops.runtime.options.exception;
 
+import ie.ucd.clops.runtime.ClopsInternalException;
+
 /**
  * 
  * @author Fintan
  *
  */
-public class InvalidOptionPropertyValueException extends Exception {
+public class InvalidOptionPropertyValueException extends ClopsInternalException {
 
   private static final long serialVersionUID = -7650183596977067164L;
 
